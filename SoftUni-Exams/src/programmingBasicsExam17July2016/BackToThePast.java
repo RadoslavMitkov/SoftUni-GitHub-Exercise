8edaf49fc5668b6a5ab8efe0cc0f8504844d2506
Radoslav Mitkov <radolinok@gmail.com>
2017-05-30 19:44:11 +0300

    changed back to the past from master branch

--- SoftUni-Exams/src/programmingBasicsExam17July2016/BackToThePast.java
+++ SoftUni-Exams/src/programmingBasicsExam17July2016/BackToThePast.java
@@ -2,25 +2,4 @@ package programmingBasicsExam17July2016;
 
 
 
-public class BackToThePast {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        double money = Double.parseDouble(scanner.nextLine());
-        int year = Integer.parseInt(scanner.nextLine());
-        int age = 18;
-
-        for (int i = 1800; i <= year; i++) {
-            if (i % 2 == 0) {
-                money -= 12000;
-            } else {
-                money -= 12000 + 50 * age;
-            }
-
-            age++;
-        }
-
-        if (money >= 0) {
-            System.out.printf("Yes! He will live a carefree life and will have %.02f dollars left.", money);
-        } else {
-            System.out.printf("He will need %.02f dollars to survive.", Math.abs(money));
+pku                                                          bibibib
